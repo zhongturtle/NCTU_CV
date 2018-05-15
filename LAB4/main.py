ee526b70b1c94d7a896e9454b39677987e61e915
zhongturtle <zhongturtle@gmail.com>
2018-05-15 18:49:35 +0800

    upload part2

--- LAB4/main.py
+++ LAB4/main.py
@@ -4,7 +4,7 @@ import os
 import argparse
 import pickle
 from glob import glob
-
+import cyvlfeat 
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
@@ -13,6 +13,10 @@ import scipy.spatial.distance as distance
 import operator
 import pdb
 import numpy as np
+from time import time
+import pdb
+from cyvlfeat.sift.dsift import dsift
+from cyvlfeat.kmeans import kmeans
 def get_data(data_path, categories, num_train_per_cat):
     num_categories = len(categories)
 
@@ -68,20 +72,6 @@ def nearest_neighbor(train_image_feats, train_labels, test_image_feats):
 
     return test_predicts
 
-DATA_PATH = 'data/'
-
-CATEGORIES = ['Kitchen', 'Store', 'Bedroom', 'LivingRoom', 'Office', 'Industrial', 'Suburb', 'InsideCity', 'TallBuilding', 'Street', 'Highway', 'OpenCountry', 'Coast', 'Mountain', 'Forest']
-
-CATE2ID = {v: k for k, v in enumerate(CATEGORIES)}
-
-ABBR_CATEGORIES = ['Kitchen', 'Store', 'Bedroom', 'LivingRoom', 'Office', 'Industrial', 'Suburb', 'InsideCity', 'TallBuilding', 'Street', 'Highway', 'OpenCountry', 'Coast', 'Mountain', 'Forest']
-
-FEATURE  = 'tiny_image'
-
-CLASSIFIER = 'nearest_neighbor'
-
-NUM_TRAIN_PER_CAT = 100
-
 def build_confusion_mtx(test_labels_ids, predicted_categories, abbr_categories):
 
     cm = confusion_matrix(test_labels_ids, predicted_categories)
@@ -105,17 +95,103 @@ def plot_confusion_matrix(cm, category, title='Confusion matrix', cmap=plt.cm.Bl
     plt.ylabel('Ground_Truth label')
     plt.xlabel('Predicted label')
  
+def build_vocabulary(image_paths, vocab_size):
+
+    feature_bag = []
+    
+    print("SIFT features extracting")
+    for image_path in image_paths:
+        image = np.asarray(Image.open(image_path),dtype='float32')
+        frames, descriptors = dsift(image, step=[5,5], fast=True)
+        feature_bag.append(descriptors)
+    feature_bag = np.concatenate(feature_bag, axis=0).astype('float32')
+    print("Computing vocabulary")
+    vocabulary = kmeans(feature_bag, vocab_size, initialization="PLUSPLUS")        
+
+    return vocabulary
+
+def get_bags_of_sifts(image_paths):
+
+    with open('vocab.pkl', 'rb') as vocab:
+        vocabulary = pickle.load(vocab)
+        image_feature = np.zeros((len(image_paths),len(vocabulary)))
+        
+    for i, path in enumerate(image_paths):
+        
+        image = np.asarray(Image.open(path), dtype = 'float32')
+        frames, descriptors = dsift(image, step=[9,9], fast=True)
+        
+        dist = distance.cdist(vocabulary, descriptors, 'euclidean')
+        mdist = np.argmin(dist, axis = 0)
+        histo, bins = np.histogram(mdist, range(len(vocabulary)+1))
+        if np.linalg.norm(histo) != 0:
+            image_feature[i, :] = histo / np.linalg.norm(histo)        
+        elif np.linalg.norm(histo) == 0:
+            image_feature[i, :] = histo
+        else:
+            print("something wrong, check the np")
+            
+    return image_feature
+
+DATA_PATH = 'data/'
+
+CATEGORIES = ['Kitchen', 'Store', 'Bedroom', 'LivingRoom', 'Office', 'Industrial', 'Suburb', 'InsideCity', 'TallBuilding', 'Street', 'Highway', 'OpenCountry', 'Coast', 'Mountain', 'Forest']
+
+CATE2ID = {v: k for k, v in enumerate(CATEGORIES)}
+
+ABBR_CATEGORIES = ['Kitchen', 'Store', 'Bedroom', 'LivingRoom', 'Office', 'Industrial', 'Suburb', 'InsideCity', 'TallBuilding', 'Street', 'Highway', 'OpenCountry', 'Coast', 'Mountain', 'Forest']
+
+#FEATURE  = 'tiny_image'
+FEATURE = 'bag_of_sift'
+CLASSIFIER = 'nearest_neighbor'
+
+NUM_TRAIN_PER_CAT = 100
+
 
 def main():
 
     print("Get the data set")
     train_image, test_image, train_labels, test_labels = get_data(DATA_PATH, CATEGORIES, NUM_TRAIN_PER_CAT)
-    print("Make training and testing dada feature")
-    train_image_feats = make_tiny_image(train_image)
-    test_image_feats = make_tiny_image(test_image)
+    if FEATURE  == 'tiny_image':
+        print("Make tiny image training and testing dada feature")
+        train_image_feats = make_tiny_image(train_image)
+        test_image_feats = make_tiny_image(test_image)
+    elif FEATURE == 'bag_of_sift':
+        print("Make bag_of_sift training and testing dada feature")
+        
+        if os.path.isfile('vocab.pkl') is True:
+            print("reuse the \" vocab.pkl\"") 
+        #it take about 40 min to compute, i upload the result, do not compute again XD 
+        
+        elif os.path.isfile('vocab.pkl') is False:
+            print('Can not fount vocub.pkl !!! Make one from dataset\n')
+            start_time = time()
+            vocabulary_size = 400
+            vocabulary = build_vocabulary(train_image, vocab_size)
+            with open('vocab.pkl', 'wb') as handle:
+                pickle.dump(vocabulary, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            finish_time = time()
+            print('it take ', finish_time - start_time , "sec to finish computing")
 
-    print("Training....")
-    predicted_categories = nearest_neighbor(train_image_feats, train_labels, test_image_feats)
+        train_image_feats = get_bags_of_sifts(train_image);
+        with open('train_image_feats.pkl', 'wb') as handle:
+            pickle.dump(train_image_feats, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        test_image_feats  = get_bags_of_sifts(test_image);
+        with open('test_image_feats.pkl', 'wb') as handle:
+            pickle.dump(test_image_feats, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+
+    if CLASSIFIER == 'nearest_neighbor':
+        print("Training....")
+        predicted_categories = nearest_neighbor(train_image_feats, train_labels, test_image_feats)
+    
+    
+    
+    
+    
+    
     
     print("Testing....")
     accuracy = float(len([x for x in zip(test_labels,predicted_categories) if x[0]== x[1]]))/float(len(test_labels))
